d40342fc2dd283bcb5e40e42b74f41c6f4b49624
Mobolaji Abdulsalam <56216197+moriire@users.noreply.github.com>
2019-11-02 06:03:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(
-  name = 'PyPaystack',  
-  packages = ['PyPaystack'],   
+  name = 'PyPaystackAPI',  
+  packages = ['PyPaystackAPI'],   
   version = '1.0',
   license='MIT',    
   description = 'A python API to easily access important paystack services.'
   author = 'Mobolaji Abdulsalam',                   
   author_email = 'ibraheemabdulsalam@gmail.com',
-  url = 'https://github.com/moriire/PyPaystack',  
-  download_url = 'https://github.com/moriire/PyPaystack/archive/v_10.tar.gz'
+  url = 'https://github.com/moriire/PyPaystackAPI',  
+  download_url = 'https://github.com/moriire/PyPaystackAPI/archive/v_10.tar.gz'
   keywords = ['python API','python payment wrapper','paystack payment API','python payment library', 'python_paystack', 'python_paystack.objects'],
   install_requires=['requests'],
   classifiers=[
